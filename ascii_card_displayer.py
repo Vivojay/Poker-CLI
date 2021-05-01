e430b04f73d1049022ff7465c66f3b9dbfd7be95
Vivo Jay <67545205+Vivojay@users.noreply.github.com>
2021-05-01 15:42:49 +0530

    Add files via upload

---
+++ ascii_card_displayer.py
@@ -0,0 +1,126 @@
+class Card(object):
+
+    card_values = {
+        'Ace': 11,  # value of the ace is high until it needs to be low
+        '2': 2,
+        '3': 3,
+        '4': 4,
+        '5': 5,
+        '6': 6,
+        '7': 7,
+        '8': 8,
+        '9': 9,
+        '10': 10,
+        'Jack': 10,
+        'Queen': 10,
+        'King': 10
+    }
+
+    def __init__(self, suit, rank):
+        """
+        :param suit: The face of the card, e.g. Spade or Diamond
+        :param rank: The value of the card, e.g 3 or King
+        """
+        self.suit = suit.capitalize()
+        self.rank = rank
+        self.points = self.card_values[rank]
+
+rank_dict = {
+    'A': 'Ace',
+    'K': 'King',
+    'Q': 'Queen',
+    'J': 'Jack',
+    'T': '10',
+    '9': '9',
+    '8': '8',
+    '7': '7',
+    '6': '6',
+    '5': '5',
+    '4': '4',
+    '3': '3',
+    '2': '2'
+}
+
+letter2suit = {
+    'S': 'Spades',
+    'D': 'Diamonds',
+    'H': 'Hearts',
+    'C': 'Clubs'
+}
+
+CARD = """\
+┌─────────┐
+│{}       │
+│         │
+│         │
+│    {}   │
+│         │
+│         │
+│       {}│
+└─────────┘
+""".format('{rank: <2}', '{suit: <2}', '{rank: >2}')
+
+HIDDEN_CARD = """\
+┌─────────┐
+│░░░░░░░░░│
+│░░░░░░░░░│
+│░░░░░░░░░│
+│░░░░░░░░░│
+│░░░░░░░░░│
+│░░░░░░░░░│
+│░░░░░░░░░│
+└─────────┘
+"""
+
+def join_lines(strings):
+    """
+    Stack strings horizontally.
+    This doesn't keep lines aligned unless the preceding lines have the same length.
+    :param strings: Strings to stack
+    :return: String consisting of the horizontally stacked input
+    """
+    liness = [string.splitlines() for string in strings]
+    return '\n'.join(''.join(lines) for lines in zip(*liness))
+
+def ascii_version_of_card(*cards):
+    """
+    Instead of a boring text version of the card we render an ASCII image of the card.
+    :param cards: One or more card objects
+    :return: A string, the nice ascii version of cards
+    """
+
+    # we will use this to prints the appropriate icons for each card
+    name_to_symbol = {
+        'Spades':   '♠',
+        'Diamonds': '♦',
+        'Hearts':   '♥',
+        'Clubs':    '♣'
+    }
+
+    def card_to_string(card):
+        # 10 is the only card with a 2-char rank abbreviation
+        rank = card.rank if card.rank == '10' else card.rank[0]
+
+        # add the individual card on a line by line basis
+        return CARD.format(rank=rank, suit=name_to_symbol[card.suit])
+
+
+    return join_lines(map(card_to_string, cards))
+
+
+def ascii_version_of_hidden_card(*cards):
+    """
+    Essentially the dealers method of print ascii cards. This method hides the first card, shows it flipped over
+    :param cards: A list of card objects, the first will be hidden
+    :return: A string, the nice ascii version of cards
+    """
+
+    return join_lines((HIDDEN_CARD, ascii_version_of_card(*cards[1:])))
+
+def dispCards(*Cards):
+    CARDS = [ Card(letter2suit[i[1]], rank_dict[i[0]]) for i in Cards]
+    print(ascii_version_of_card(*CARDS))
+
+def _dispCards(*Cards):
+    CARDS = [ Card(letter2suit[i[1]], rank_dict[i[0]]) for i in Cards]
+    return (ascii_version_of_card(*CARDS))
---
+++ email_cards.py
@@ -0,0 +1,58 @@
+# Visit https://www.google.com/settings/security/lesssecureapps and "Turn ON"
+#Add your gmail ID and password where it asks for 'sender's gmail ID and password' 
+
+#Easiest and Readable way to Email
+#through Python SMTPLIB library
+#This works with >>> Gmail.com <<<
+import smtplib
+import json, smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+from email.message import EmailMessage
+from ascii_card_displayer import _dispCards
+
+from unicards import unicard as uc
+
+with open('res/holes.json') as f:
+    Hole_Cards = json.load(f)
+
+with open('res/emails.json') as f:
+    Email_IDs = json.load(f)
+
+def CARD(*aas):
+    return '&nbsp;'.join([uc(aa.replace('10', 'T')[0]+aa.replace('10', 'T')[1].lower()) for aa in aas])
+
+EmailAdd = "" #senders Gmail id over here
+Pass = "" #senders Gmail's Password over here
+
+server = smtplib.SMTP(host='smtp.gmail.com', port=587)
+server.ehlo()
+server.starttls()
+server.login(EmailAdd, Pass)
+
+def SEND():
+    for i, EID in zip( list(Hole_Cards.values()), Email_IDs ):
+        msg = MIMEMultipart('alternative')
+
+        EmailContent = CARD(*i)
+        EmailContent = '''
+        <html>
+        
+        <p style = "font-family: Courier New; font-size: 268px">
+        '''+EmailContent+'''
+        
+        </p>
+        </html>
+        '''
+
+        HTML = MIMEText(EmailContent, 'html')
+        msg.attach(HTML)
+
+        msg['Subject'] = 'Your poker hole cards'
+        msg['From'] = EmailAdd
+        msg['To'] = EID
+
+        server.sendmail(EmailAdd, EID, msg.as_string())
+
+    server.quit()
