3131446e06cc944d1b9a326c4d17713830499f96
Vivo Jay <67545205+Vivojay@users.noreply.github.com>
2021-05-02 14:01:10 +0530

    Update email_cards.py

--- email_cards.py
+++ email_cards.py
@@ -1,9 +1,10 @@
 # Visit https://www.google.com/settings/security/lesssecureapps and "Turn ON"
-#Add your gmail ID and password where it asks for 'sender's gmail ID and password' 
 
 #Easiest and Readable way to Email
 #through Python SMTPLIB library
 #This works with >>> Gmail.com <<<
+
+import os
 import smtplib
 import json, smtplib
 from email.mime.multipart import MIMEMultipart
@@ -14,24 +15,25 @@ from ascii_card_displayer import _dispCards
 
 from unicards import unicard as uc
 
-with open('res/holes.json') as f:
-    Hole_Cards = json.load(f)
-
-with open('res/emails.json') as f:
-    Email_IDs = json.load(f)
-
-def CARD(*aas):
-    return '&nbsp;'.join([uc(aa.replace('10', 'T')[0]+aa.replace('10', 'T')[1].lower()) for aa in aas])
-
-EmailAdd = "" #senders Gmail id over here
-Pass = "" #senders Gmail's Password over here
-
-server = smtplib.SMTP(host='smtp.gmail.com', port=587)
-server.ehlo()
-server.starttls()
-server.login(EmailAdd, Pass)
 
 def SEND():
+    with open('res/holes.json') as f:
+        Hole_Cards = json.load(f)
+
+    with open('res/emails.json') as f:
+        Email_IDs = json.load(f)
+
+    def CARD(*aas):
+        return '&nbsp;'.join([uc(aa.replace('10', 'T')[0]+aa.replace('10', 'T')[1].lower()) for aa in aas])
+
+    EmailAdd = os.environ['GMAIL_ID'] #senders Gmail id over here
+    Pass = os.environ['GMAIL_PASS'] #senders Gmail's Password over here
+
+    server = smtplib.SMTP(host='smtp.gmail.com', port=587)
+    server.ehlo()
+    server.starttls()
+    server.login(EmailAdd, Pass)
+
     for i, EID in zip( list(Hole_Cards.values()), Email_IDs ):
         msg = MIMEMultipart('alternative')
 
